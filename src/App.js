4f721906ff17c3d71be82759b31da8a343792e66
YoonCode <yjin830201@gmail.com>
2022-07-16 22:02:27 +0900

    input and upload names

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 
 function App() {
-  const [time, setTime] = useState(1)
+  const [names, setNames] = useState(['홍길동'])
+  const [input, setInput] = useState('')
 
-  const handleClick = () => {
-    let newTime
-    if (time >= 12) {
-      newTime = 1
-    } else {
-      newTime = time + 1
-    }
-    setTime(newTime)
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      return [input, ...prevState]
+    })
+    setInput('')
   }
 
   return (
     <>
-      <span>현재시각: {time}시</span>
-      <button onClick={handleClick}>Update</button>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>
+      })}
     </>
   )
 }
