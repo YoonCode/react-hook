2cdd1df0cc3fca72eacb7a15679ea403aca97c5b
YoonCode <yjin830201@gmail.com>
2022-07-17 12:33:15 +0900

    understand when useRef is effective

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const App = () => {
-  const [renderer, setRenderer] = useState(0)
-  const countRef = useRef(0) // 생애주기 동안 유지
-  let countVar = 0 // 렌더링 할 때마다 초기화
+  const [count, setCount] = useState(1)
+  const renderCount = useRef(1)
 
-  const doRendering = () => {
-    setRenderer(renderer + 1)
-  }
-
-  const increaseRef = () => {
-    countRef.current += 1
-    console.log('ref: ', countRef.current)
-  }
-
-  const increaseVar = () => {
-    countVar += 1
-    console.log('var: ', countVar)
-  }
-
-  const printResults = () => {
-    console.log(`ref: ${countRef.current}, var: ${countVar}`)
-  }
+  useEffect(() => {
+    console.log('렌더링 수: ', renderCount.current)
+    renderCount.current += 1
+  })
 
   return (
     <>
-      <p>Ref: {countRef.current}</p>
-      <p>Var: {countVar}</p>
-      <button onClick={doRendering}>렌더!</button>
-      <button onClick={increaseRef}>Ref 올려</button>
-      <button onClick={increaseVar}>Var 올려</button>
-      <button onClick={printResults}>Ref Var 값 출력</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>올려</button>
     </>
   )
 }
