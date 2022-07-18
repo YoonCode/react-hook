1edbb042095e819096b1b00b9a8686487ebf65fe
YoonCode <yjin830201@gmail.com>
2022-07-18 22:35:05 +0900

    compare useRef to useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(1)
-  const renderCount = useRef(1)
+  const [count, setCount] = useState(0)
+  const countRef = useRef(0)
 
-  useEffect(() => {
-    console.log('렌더링 수: ', renderCount.current)
-    renderCount.current += 1
-  })
+  // console.log(countRef) // countRef.current
+
+  console.log('랜더링...')
+
+  const increaseCountState = () => {
+    // state 값이 변할 때마다 랜더링이 일어남.
+    setCount(count + 1)
+  }
+
+  const increaseCountRef = () => {
+    // ref 값이 변해도 랜더링이 일어나지 않음. 하지만 전 생애주기 동안 값을 유지함.
+    countRef.current += 1
+    console.log('Ref: ', countRef.current)
+  }
 
   return (
     <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>올려</button>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseCountState}>State 올려</button>
+      <button onClick={increaseCountRef}>Ref 올려</button>
     </>
   )
 }
