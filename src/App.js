5ed0a37f38d203462deed785cb961dc5af25b680
YoonCode <yjin830201@gmail.com>
2022-07-19 00:08:49 +0900

    create ThemeContext to use useContext instead of props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import './App.css'
 import Page from './components/Page'
+import { ThemeContext } from './context/ThemeContext'
 
 const App = () => {
   const [isDark, setIsDark] = useState(false)
-  return <Page isDark={isDark} setIsDark={setIsDark} />
+  return (
+    // ThemeContext.Provider 가 감싸는 모든 하위 component 는 value 에 접근 가능함.
+    <ThemeContext.Provider value={{ isDark, setIsDark }}>
+      <Page />
+    </ThemeContext.Provider>
+  )
 }
 
 export default App
