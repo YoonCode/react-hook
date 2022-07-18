9ea1c38de48c5b672262c134044a396df96ea90f
YoonCode <yjin830201@gmail.com>
2022-07-18 23:08:35 +0900

    when is useRef useful

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const App = () => {
-  const [renderer, setRenderer] = useState(0)
-  const countRef = useRef(0)
-  let countVar = 0
+  const [count, setCount] = useState(1)
+  const renderCount = useRef(1)
 
-  const doRendering = () => {
-    setRenderer(renderer + 1)
-  }
-
-  const increaseRef = () => {
-    // ref 값은 함수가 재실행되어도 유지됨.
-    countRef.current += 1
-    console.log('Ref: ', countRef.current)
-  }
-
-  const increaseVar = () => {
-    // var 값은 함수가 재실행되면 초기화됨.
-    countVar += 1
-    console.log('Var: ', countVar)
-  }
-
-  const printResults = () => {
-    console.log(`ref: ${countRef.current}, var: ${countVar}`)
-  }
+  useEffect(() => {
+    // 랜더링을 발생시키면 안되는 경우에 useRef 가 유용함.
+    console.log('랜더링 수: ', renderCount.current)
+    renderCount.current += 1
+  })
 
   return (
     <>
-      <p>Ref: {countRef.current}</p>
-      <p>Var: {countVar}</p>
-      <button onClick={doRendering}>랜더!!</button>
-      <button onClick={increaseRef}>Ref 올려</button>
-      <button onClick={increaseVar}>Var 올려</button>
-      <button onClick={printResults}>Ref Var 값 출력</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>올려</button>
     </>
   )
 }
