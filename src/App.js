bd2b39d41e10810074386344805c2f4ae1f977b1
YoonCode <yjin830201@gmail.com>
2022-07-19 23:28:54 +0900

    object values cause issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ const App = () => {
   const [number, setNumber] = useState(0)
   const [isKorea, setIsKorea] = useState(true)
 
-  const location = isKorea ? '한국' : '외국'
+  // const location = isKorea ? '한국' : '외국'
+  const location = {
+    country: isKorea ? '한국' : '외국',
+  }
 
   useEffect(() => {
     console.log('useEffect 호출')
@@ -20,7 +23,7 @@ const App = () => {
       />
       <hr />
       <h2>어느 나라에 있어요?</h2>
-      <p>나라: {location}</p>
+      <p>나라: {location.country}</p>
       <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
   )
