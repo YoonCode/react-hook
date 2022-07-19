335f775e3b7df84f6f92b48de16150ebdcb94abc
YoonCode <yjin830201@gmail.com>
2022-07-19 23:09:14 +0900

    after rendering, values in function would be initialized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react'
-import './App.css'
-import Page from './components/Page'
-import { ThemeContext } from './context/ThemeContext'
-import { UserContext } from './context/UserContext'
+
+const hardCalculate = (number) => {
+  console.log('어려운 계산!')
+  for (let i = 0; i < 999999999; i++) {} // 생각하는 시간
+  return number + 10000
+}
+const easyCalculate = (number) => {
+  console.log('짱 쉬운 계산!')
+  return number + 1
+}
 
 const App = () => {
-  const [isDark, setIsDark] = useState(false)
+  const [hardNumber, setHardNumber] = useState(1)
+  const [easyNumber, setEasyNumber] = useState(1)
+
+  const hardSum = hardCalculate(hardNumber)
+  const easySum = easyCalculate(easyNumber)
+
   return (
-    // ThemeContext.Provider 가 감싸는 모든 하위 component 는 value 에 접근 가능함.
-    <UserContext.Provider value={'사용자'}>
-      <ThemeContext.Provider value={{ isDark, setIsDark }}>
-        <Page />
-      </ThemeContext.Provider>
-    </UserContext.Provider>
+    <div>
+      <h3>어려운 계산기</h3>
+      <input
+        type="number"
+        value={hardNumber}
+        onChange={(e) => setHardNumber(parseInt(e.target.value))}
+      />
+      <span> + 10000 = {hardSum}</span>
+      <h3>쉬운 계산기</h3>
+      <input
+        type="number"
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
+    </div>
   )
 }
 
