3a91d26623bbb34a8e5a166b4f372366795365f8
YoonCode <yjin830201@gmail.com>
2022-07-20 09:59:53 +0900

    add useCallback

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 const App = () => {
   const [number, setNumber] = useState(0)
-  const [isKorea, setIsKorea] = useState(true)
 
-  // const location = isKorea ? '한국' : '외국'
-
-  // const location = {
-  //   country: isKorea ? '한국' : '외국',
+  // someFunction에 할당된 함수(객체)의 주소는 리렌더링될 때마다 바뀜.(재사용 안됨.)
+  // const someFunction = () => {
+  //   console.log(`someFunc: number: ${number}`)
+  //   return
   // }
 
-  const location = useMemo(() => {
-    return {
-      country: isKorea ? '한국' : '외국',
-    }
-  }, [isKorea])
+  // 이제 someFunction에는 memoization된 함수의 주소가 들어감.(재사용 가능.)
+  const someFunction = useCallback(() => {
+    console.log(`someFunc: number: ${number}`)
+    return
+  }, [number])
 
   useEffect(() => {
-    console.log('useEffect 호출')
-  }, [location])
+    console.log('someFunction이 변경되었습니다.')
+  }, [someFunction])
 
   return (
     <div>
-      <h2>하루에 몇 끼 먹어요?</h2>
       <input
         type="number"
         value={number}
         onChange={(e) => setNumber(e.target.value)}
       />
-      <hr />
-      <h2>어느 나라에 있어요?</h2>
-      <p>나라: {location.country}</p>
-      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+      <br />
+      <button onClick={someFunction}>Call someFunc</button>
     </div>
   )
 }
