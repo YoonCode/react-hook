e08f260102d23d02d16c80e9171f887b3d59b609
YoonCode <yjin830201@gmail.com>
2022-07-20 23:31:42 +0900

    create box size changer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState } from 'react'
+import Box from './Box'
 
 const App = () => {
-  const [number, setNumber] = useState(0)
-  const [toggle, setToggle] = useState(true)
+  const [size, setSize] = useState(100)
 
-  // someFunction에 할당된 함수(객체)의 주소는 리렌더링될 때마다 바뀜.(재사용 안됨.)
-  // const someFunction = () => {
-  //   console.log(`someFunc: number: ${number}`)
-  //   return
-  // }
-
-  // 이제 someFunction에는 memoization된 함수의 주소가 들어감.(재사용 가능.)
-  const someFunction = useCallback(() => {
-    console.log(`someFunc: number: ${number}`)
-    return
-  }, [number])
-
-  useEffect(() => {
-    console.log('someFunction이 변경되었습니다.')
-  }, [someFunction])
+  const createBoxStyle = () => {
+    return {
+      backgroundColor: 'pink',
+      width: `${size}px`,
+      height: `${size}px`,
+    }
+  }
 
   return (
     <div>
       <input
         type="number"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
+        value={size}
+        onChange={(e) => {
+          setSize(e.target.value)
+        }}
       />
-      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
-      <br />
-      <button onClick={someFunction}>Call someFunc</button>
+      <Box createBoxStyle={createBoxStyle} />
     </div>
   )
 }
